1eff32f928a392645b32ef0ad99939f5845a9e71
tubisaffo <tubisaffo@gmail.com>
2024-05-24 11:33:26 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+happy_new_year()    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x**2 for x in int_list]
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = square_integers(numbers)
+print(squared_numbers)
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
